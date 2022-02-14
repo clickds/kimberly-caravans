30999b8149c8e1d3abb019c45aacf4a3d36463a6
Glen Elkins <glenelkinsdev@gmail.com>
2022-02-14 14:14:58 +0000

    Updated readme

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -2,6 +2,8 @@
 <html>
     <head>
         <title>Title Here</title>
+        <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}" />
     </head>
     <body>
         @include('components.nav-bar')
